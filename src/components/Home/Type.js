516e174029bdc064c08ffc56f2d6850614a3c5c2
xtian <129867367+engrxtian@users.noreply.github.com>
2023-10-04 12:05:48 +0800

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,6 @@ function Type() {
       options={{
         strings: [
           "Full-stack Web Developer",
-          "Wordpress Developer",
           "Freelancer",
         ],
         autoStart: true,
